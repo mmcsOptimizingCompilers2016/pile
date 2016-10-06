555ca49d55ac001e20141282150e2974fac674e3
Vlad Yanushka <vlad.yanushka@gmail.com>
2016-10-06 15:11:45 +0300

    Adding classes for representation three address code

---
+++ OptimizingCompilers2016/Library/ThreeAddressCode/Base/BaseThreeAddressCode.cs
@@ -0,0 +1,11 @@
+﻿namespace OptimizingCompilers2016.Library.ThreeAddressCode.Base
+{
+    public abstract class BaseThreeAddressCode : IThreeAddressCode
+    {
+        public abstract object Destination { get; set; }
+        public abstract object Label { get; set; }
+        public abstract object LeftOperand { get; set; }
+        public abstract Operation Operation { get; set; }
+        public abstract object RightOperand { get; set; }
+    }
+}
---
+++ OptimizingCompilers2016/Library/ThreeAddressCode/Values/Base/BaseValue.cs
@@ -0,0 +1,7 @@
+﻿namespace OptimizingCompilers2016.Library.ThreeAddressCode.Values.Base
+{
+    public abstract class BaseValue<T>
+    {
+        public T Value { get; set; }
+    }
+}
