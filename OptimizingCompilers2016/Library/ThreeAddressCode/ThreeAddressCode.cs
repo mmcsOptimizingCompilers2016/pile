e255bc9f2691e2926b2a6d1c9ab836e5c5bdc33f
Vlad Yanushka <vlad.yanushka@gmail.com>
2016-10-06 15:25:20 +0300

    Changing BaseThreeAddressCode

---
+++ OptimizingCompilers2016/Library/ThreeAddressCode/ThreeAddressCode.cs
@@ -0,0 +1,13 @@
+﻿namespace OptimizingCompilers2016.Library.ThreeAddressCode
+{
+    using Values;
+
+    public abstract class ThreeAddressCode : IThreeAddressCode
+    {
+        public object Destination { get; set; }
+        public LabelValue Label { get; set; }
+        public object LeftOperand { get; set; }
+        public Operation Operation { get; set; }
+        public object RightOperand { get; set; }
+    }
+}
