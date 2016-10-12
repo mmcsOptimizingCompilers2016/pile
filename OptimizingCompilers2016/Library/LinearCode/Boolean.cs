cbe677bbfd27c75017e09f138e642d9b92e49fa6
Helen Gordienko <gloomylego@gmail.com>
2016-10-13 01:10:33 +0300

    WIP: remove old basic classes

--- OptimizingCompilers2016/Library/LinearCode/Boolean.cs
+++
@@ -1,9 +0,0 @@
-namespace OptimizingCompilers2016.Library.LinearCode
-{
-    public class Boolean : Value
-    {
-        public Boolean(bool b) { boolean = b; }
-        public bool boolean;
-        public override string ToString() { return boolean.ToString(); }
-    }
-}
