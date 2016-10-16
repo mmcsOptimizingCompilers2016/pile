49263e38c61cb564f8ba13d2660412bf41ac2eec
bianconeri23 <787898avladik23>
2016-10-16 19:25:37 +0300

    GUI

--- OptimizingCompilers2016/GUI/Form1.Designer.cs
+++ OptimizingCompilers2016/GUI/Form1.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Code = new System.Windows.Forms.RichTextBox();
+            this.Run = new System.Windows.Forms.Button();
+            this.ResultCode = new System.Windows.Forms.RichTextBox();
+            this.Console = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Code
+            // 
+            this.Code.Location = new System.Drawing.Point(12, 27);
+            this.Code.Name = "Code";
+            this.Code.Size = new System.Drawing.Size(428, 442);
+            this.Code.TabIndex = 0;
+            this.Code.Text = "";
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(446, 220);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(75, 39);
+            this.Run.TabIndex = 1;
+            this.Run.Text = "Пуск";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // ResultCode
+            // 
+            this.ResultCode.Location = new System.Drawing.Point(527, 27);
+            this.ResultCode.Name = "ResultCode";
+            this.ResultCode.Size = new System.Drawing.Size(428, 442);
+            this.ResultCode.TabIndex = 2;
+            this.ResultCode.Text = "";
+            // 
+            // Console
+            // 
+            this.Console.Location = new System.Drawing.Point(12, 487);
+            this.Console.Name = "Console";
+            this.Console.Size = new System.Drawing.Size(943, 104);
+            this.Console.TabIndex = 3;
+            this.Console.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Menu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(962, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Menu
+            // 
+            this.Menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuOpen,
+            this.MenuExit});
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(48, 20);
+            this.Menu.Text = "Файл";
+            // 
+            // MenuOpen
+            // 
+            this.MenuOpen.Name = "MenuOpen";
+            this.MenuOpen.Size = new System.Drawing.Size(121, 22);
+            this.MenuOpen.Text = "Открыть";
+            this.MenuOpen.Click += new System.EventHandler(this.MenuOpen_Click);
+            // 
+            // MenuExit
+            // 
+            this.MenuExit.Name = "MenuExit";
+            this.MenuExit.Size = new System.Drawing.Size(121, 22);
+            this.MenuExit.Text = "Выход";
+            this.MenuExit.Click += new System.EventHandler(this.MenuExit_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(962, 603);
+            this.Controls.Add(this.Console);
+            this.Controls.Add(this.ResultCode);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.Code);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "GUI";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox Code;
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.RichTextBox ResultCode;
+        private System.Windows.Forms.RichTextBox Console;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Menu;
+        private System.Windows.Forms.ToolStripMenuItem MenuOpen;
+        private System.Windows.Forms.ToolStripMenuItem MenuExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- OptimizingCompilers2016/GUI/Form1.cs
+++ OptimizingCompilers2016/GUI/Form1.cs
@@ -7,14 +7,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using OptimizingCompilers2016.Library;
+using OptimizingCompilers2016.Library.Helpers;
+using OptimizingCompilers2016.Library.LinearCode;
+using OptimizingCompilers2016.Library.Visitors;
+
 
 namespace OptimizingCompilers2016.GUI
 {
     public partial class Form1 : Form
     {
+
+        string FileName;
         public Form1()
         {
             InitializeComponent();
+            FileName = @"a.txt";
+
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string text = File.ReadAllText(FileName);
+               
+                Scanner scanner = new Scanner();
+                scanner.SetSource(text, 0);
+
+                Parser parser = new Parser(scanner);
+
+                var b = parser.Parse();
+
+                var linearCode = new LinearCodeVisitor();
+                parser.root.Accept(linearCode);
+
+                ResultCode.Text = linearCode.ToString();
+
+
+            }
+            catch (FileNotFoundException)
+            {
+                Console.Text += String.Format("Файл {0} не найден!\r\n", FileName);
+
+            }
+            catch (LexException exception)
+            {
+                Console.Text += String.Format("Лексическая ошибка. " + exception.Message+ "\r\n");
+            }
+            catch (SyntaxException exception)
+            {
+                Console.Text += String.Format("Синтаксическая ошибка. " + exception.Message+ "\r\n");
+            }
+
+
+            System.Console.ReadLine();
+        }
+
+        private void MenuExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuOpen_Click(object sender, EventArgs e)
+        {
+            FileName = this.openFileDialog1.FileName;
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FileName = this.openFileDialog1.FileName;
+
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+                Code.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+
+
         }
     }
 }
