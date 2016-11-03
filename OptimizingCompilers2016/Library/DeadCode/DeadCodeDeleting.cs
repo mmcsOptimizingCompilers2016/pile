21c53608ce09366b86687e11378ca1b1e7a84ac8
VladislavStaryh <vlad.star.08@gmail.com>
2016-11-03 15:19:21 +0300

    Edit module delete dead code

---
+++ OptimizingCompilers2016/Library/DeadCode/DeadCodeDeleting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizingCompilers2016.Library.DeadCode
+{
+    class DeadCodeDeleting
+    {
+        
+        BaseBlock.BaseBlock optimizeDeadCode(BaseBlock.BaseBlock block)
+        {
+            throw new NotImplementedException("Not implemented deleting dead code");
+
+            
+
+            return block;
+        }
+
+    }
+}
