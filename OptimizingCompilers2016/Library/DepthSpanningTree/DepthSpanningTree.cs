b48458a764dade2e6e057d40be463cb4125d18ab
Elena Gordienko <gloomylego@gmail.com>
2016-11-17 22:25:04 +0300

    add constructor for depth spanning tree

--- OptimizingCompilers2016/Library/DepthSpanningTree/DepthSpanningTree.cs
+++ OptimizingCompilers2016/Library/DepthSpanningTree/DepthSpanningTree.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using QuickGraph;
+using QuickGraph.Graphviz;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,20 +14,47 @@ namespace OptimizingCompilers2016.Library.DepthSpanningTree
     using ControlFlowGraph = ControlFlowGraph.ControlFlowGraph;
 
     class DepthSpanningTree
-    {
+    {	
         HashSet<BaseBlock> Visited;
+		Dictionary<BaseBlock, int> Numbers;
+		BidirectionalGraph<BaseBlock, Edge<BaseBlock>> SpanningTree;
 
-        public DepthSpanningTree(ControlFlowGraph cfg)
+		/// <summary>
+		/// Depth spanning tree constructor. 
+		/// Bilds enumeration of blocks in reverse order.
+		/// </summary>
+		/// <param name="cfg">control flow graph</param>
+        public DepthSpanningTree(ControlFlowGraph controlFlowGraph)
         {   
-            int numberOfVertices = cfg.NumberOfVertices();
+            int numberOfVertices = controlFlowGraph.NumberOfVertices() - 1;
             Visited = new HashSet<BaseBlock>();
+			SpanningTree = new BidirectionalGraph<BaseBlock, Edge<BaseBlock>>();
 
-
+			var rootBlock = controlFlowGraph.GetRoot();
+			BuildTree(rootBlock, ref numberOfVertices);
         }
 
-        private void BuildTree(BaseBlock block)
+		/// <summary>
+		/// Auxiliary recursive function for building depth spanning tree
+		/// </summary>
+		/// <param name="block">current block</param>
+		/// <param name="currentNumber">counter for enumeration of blocks</param>
+        private void BuildTree(BaseBlock block, ref int currentNumber)
         {
+			if (block == null)
+				return;
+
             Visited.Add(block);
+			foreach (var predecessor in block.Predecessors)
+			{
+				if ( !Visited.Contains(predecessor) )
+				{
+					SpanningTree.AddEdge(new Edge<BaseBlock>(block, predecessor));
+					BuildTree(predecessor, ref currentNumber);
+				}
+				Numbers[block] = currentNumber;
+				currentNumber -= 1;
+			}
         }
     }
 }
