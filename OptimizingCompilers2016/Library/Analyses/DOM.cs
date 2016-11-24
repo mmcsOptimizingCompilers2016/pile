d3d1137b2c84d05840b4b6fd5d1090c9a3ac5305
VladislavStaryh <vlad.star.08@gmail.com>
2016-11-24 14:43:24 +0300

    Started working on dom structure building

---
+++ OptimizingCompilers2016/Library/Analyses/DOM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizingCompilers2016.Library.Analyses
+{
+    public class DOM
+    {
+        public class NodeCFG
+        {
+
+        }
+
+
+        public DOM(BaseBlock.BaseBlock block)
+        {
+            
+        }
+    }
+}
