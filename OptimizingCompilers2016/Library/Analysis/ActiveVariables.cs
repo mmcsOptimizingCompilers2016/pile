fb2aef037ebce8218f81a0a3258f1b73ef81e6d9
B0pre <89094191xxx@gmail.com>
2016-12-01 15:23:12 +0300

    fix runAnalys

--- OptimizingCompilers2016/Library/Analysis/ActiveVariables.cs
+++ OptimizingCompilers2016/Library/Analysis/ActiveVariables.cs
@@ -4,18 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OptimizingCompilers2016.Library.ThreeAddressCode.Values;
+using Occurrence = System.Tuple<int, OptimizingCompilers2016.Library.ThreeAddressCode.Values.IdentificatorValue>;
+
 
 namespace OptimizingCompilers2016.Library.Analysis
 {
     public class ActiveVariables
     {
 
-        public List<HashSet<IdentificatorValue>> result = new List<HashSet<IdentificatorValue>>();
+        public new Dictionary<string, HashSet<Occurrence>> result = new Dictionary<string, HashSet<Occurrence>>();
 
-        private List<HashSet<IdentificatorValue>> IN = new List<HashSet<IdentificatorValue>>();
-        private List<HashSet<IdentificatorValue>> OUT = new List<HashSet<IdentificatorValue>>();
-        private List<HashSet<IdentificatorValue>> Def = new List<HashSet<IdentificatorValue>>();
-        private List<HashSet<IdentificatorValue>> Use = new List<HashSet<IdentificatorValue>>();
+        private Dictionary<string, HashSet<Occurrence>> IN = new Dictionary<string, HashSet<Occurrence>>();
+        private Dictionary<string, HashSet<Occurrence>> OUT = new Dictionary<string, HashSet<Occurrence>>();
+        private Dictionary<string, HashSet<Occurrence>> Def = new Dictionary<string, HashSet<Occurrence>>();
+        private Dictionary<string, HashSet<Occurrence>> Use = new Dictionary<string, HashSet<Occurrence>>();
         private List<BaseBlock> blocks;
 
         public ActiveVariables(List<BaseBlock> blocks)
@@ -29,67 +31,72 @@ namespace OptimizingCompilers2016.Library.Analysis
             for (int i = 0; i < blocks.Count; i++)
             {
                 var ldu = new InblockDefUse(blocks[i]);
-                var Set = new HashSet<IdentificatorValue>();
+                var Set = new HashSet<Occurrence>();
                 foreach (var key in ldu.result.Keys)
-                    Set.Add(new IdentificatorValue(key.Item2.Value));
-                Def.Add(Set);
+                    Set.Add(key);
+                Def[blocks[i].Name] = Set;
             }
 
             for (int i = 0; i < blocks.Count; i++)
             {
                 var ldu = new InblockDefUse(blocks[i]);
-                var Set = new HashSet<IdentificatorValue>();
+                var Set = new HashSet<Occurrence>();
                 foreach (var sets in ldu.result.Values)
                     foreach (var itemSet in sets)
-                        Set.Add(new IdentificatorValue(itemSet.Item2.Value));
-                Use.Add(Set);
+                        Set.Add(itemSet);
+                Use[blocks[i].Name] = Set;
             }
 
             for (int i = 0; i < blocks.Count; i++)
             {
-                IN.Add(new HashSet<IdentificatorValue>());
-                OUT.Add(new HashSet<IdentificatorValue>());
+                IN[blocks[i].Name] = new HashSet<Occurrence>();
+                OUT[blocks[i].Name] = new HashSet<Occurrence>();
             }
         }
 
 
-        public List<HashSet<IdentificatorValue>> runAnalys()
+        public Dictionary<string, HashSet<Occurrence>> runAnalys()
         {
-            var oldIN = new List<HashSet<IdentificatorValue>>(IN);
-            
+            var oldIN = new Dictionary<string, HashSet<Occurrence>>(IN);
+
             while (true)
             {
-                var count = 0;
+                var isChanged = false;
                 //Console.WriteLine("Ахахахха)000))");
                 for (int i = 0; i < blocks.Count; i++)
                 {
-                    OUT[i] = new HashSet<IdentificatorValue>();
-                    foreach (var predecessor in blocks[i].Predecessors)
+                    OUT[blocks[i].Name] = new HashSet<Occurrence>();
+
+                    var successors = new List<BaseBlock>();
+                    successors.Add(blocks[i].Output);
+                    successors.Add(blocks[i].JumpOutput);
+
+                    foreach (var successor in successors)
                     {
-                        int index = Convert.ToInt32(predecessor.Name.Remove(0, 1)) - 1;
-                        if (index >=0)
-                            OUT[i].UnionWith(IN[index]);
+                        if (successor != null)
+                            OUT[blocks[i].Name].UnionWith(IN[successor.Name]);
                     }
-                    OUT[i].ExceptWith(Def[i]);
-                    OUT[i].UnionWith(Use[i]);
-                    IN[i] = new HashSet<IdentificatorValue>(OUT[i]);
+
+                    OUT[blocks[i].Name].ExceptWith(Def[blocks[i].Name]);
+                    OUT[blocks[i].Name].UnionWith(Use[blocks[i].Name]);
+                    IN[blocks[i].Name] = new HashSet<Occurrence>(OUT[blocks[i].Name]);
                 }
 
                 for (int i = 0; i < oldIN.Count(); i++)
                 {
-                    if (!oldIN[i].SetEquals(IN[i]))
-                        count++;
+                    if (!oldIN[blocks[i].Name].SetEquals(IN[blocks[i].Name]))
+                        isChanged = true;
                 }
 
-                if (count == 0)
+                if (!isChanged)
                     break;
 
-                oldIN = new List<HashSet<IdentificatorValue>>(IN);
+                oldIN = new Dictionary<string, HashSet<Occurrence>>(IN);
             }
 
-            result = new List<HashSet<IdentificatorValue>>(IN);
+            result = IN;
 
-            return result;   
+            return result;
         }
 
 
