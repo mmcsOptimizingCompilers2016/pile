5b8af992bd9ca618a94dddf3e738bbf1633d3fd9
Elena Gordienko <gloomylego@gmail.com>
2016-12-15 01:25:45 +0300

    add classes for each region type

--- OptimizingCompilers2016/Library/Region/Region.cs
+++ OptimizingCompilers2016/Library/Region/Region.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using QuickGraph.Collections;
+﻿using System.Collections.Generic;
+using QuickGraph;
 
 namespace OptimizingCompilers2016.Library.Region
 {
+	using BidirectionalGraph = QuickGraph.BidirectionalGraph<Region, Edge<Region>>;
+	using ControlFlowGraph = ControlFlowGraph.ControlFlowGraph;
 	/// <summary>
 	/// Type of the region:
 	///		BaseBlock - region consisting of one base block (i.e. header)
@@ -29,25 +27,89 @@ namespace OptimizingCompilers2016.Library.Region
 	///		3) region should contain all the arcs between its nodes 
 	///			(may be except for the reverse arcs to the header)
 	/// </summary>
-	class Region
+	public class Region
 	{	
+		// TODO: decide whether to use list or graph
+		public List<Region> SourceRegions { get; }
+		public List<Region> TargetRegions { get; }
+
+		public Region()
+		{
+			SourceRegions = new List<Region>();
+			TargetRegions = new List<Region>();
+		}
+
+		public void AddTargetRegion(Region target)
+		{
+			TargetRegions.Add(target);
+		}
+
+		public void AddSourceRegion(Region source)
+		{
+			SourceRegions.Add(source);
+		}
+
+	}
+
+	public class BaseBlockRegion : Region
+	{
+		public BaseBlock Block { get; }
+
+		public BaseBlockRegion(BaseBlock block): base()
+		{
+			Block = block;
+		}
+	}
+
+	/// <summary>
+	/// Region, corresponding to cycle body (contains no cycles)
+	/// </summary>
+	public class CycleBodyRegion : Region
+	{
+		public Region CycleBodyStart { get; }
+		public BidirectionalGraph OutEdges { get; }
+		public CycleBodyRegion(Region cycleBodyStart) : base()
+		{
+			CycleBodyStart = cycleBodyStart;
+			OutEdges = new BidirectionalGraph();
+		}
+
+		public void AddOutEdge(Region source, Region target)
+		{
+			var edge = new QuickGraph.Edge<Region>(source, target);
+			OutEdges.AddEdge(edge);
+		}
+	}
+
+	/// <summary>
+	/// Region, corresponding to cycle ( cycle body + header )
+	/// </summary>
+	public class CycleRegion : Region
+	{
+		public Region CycleStart { get; }
 		/// <summary>
-		/// Type of the region
+		/// Regions, which are the start of reverse edges
 		/// </summary>
-		private RegionType Type { get; }
+		public List<Region> ReverseEdgeSource { get; }
+		public CycleRegion(Region cycleStart) : base()
+		{
+			CycleStart = cycleStart;
+			ReverseEdgeSource = new List<Region>();
+		}
 
-		/// <summary>
-		/// Node which dominates all the other nodes in this region
-		/// </summary>
-		private BaseBlock HeaderNode { get; }
+		public void AddReverseEdgeFrom(Region source)
+		{
+			ReverseEdgeSource.Add(source);
+		}
+	}
 
-		//private QuickGraph.BidirectionalGraph<Region, QuickGraph.Edge<Region> > Graph { get; }
-
-		public Region(BaseBlock header, RegionType type)
-		{	
-			// TODO: check whether all the three conditions are satisfied
-			this.HeaderNode = header;
-			this.Type = type;
+	public class RegionHierarchy
+	{
+		public BidirectionalGraph Hierarchy { get; }
+		public RegionHierarchy(ControlFlowGraph cfg)
+		{
+			Hierarchy = new BidirectionalGraph();
+			// TODO: implement me
 		}
 	}
 }
