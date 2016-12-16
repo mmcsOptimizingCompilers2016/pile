e3b85667cf5128cd5996dd0c0c830fc077f2e512
Dmitry Avtonomov <avtonomovvv@gmail.com>
2016-12-16 22:00:09 +0300

    add class

---
+++ OptimizingCompilers2016/Library/Analysis/AvailabilityAnalysis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace OptimizingCompilers2016.Library.Analysis
+{
+    public class AvailabilityAnalysis : BaseIterationAlgorithm<BitArray>
+    {
+        protected override void FillGeneratorsAndKillers(List<BaseBlock> blocks)
+        {
+
+        }
+
+        protected override BitArray SetStartingSet()
+        {
+        }
+
+        protected override BitArray SubstractSets(BitArray firstSet, BitArray secondSet)
+        {
+        }
+    }
+}
