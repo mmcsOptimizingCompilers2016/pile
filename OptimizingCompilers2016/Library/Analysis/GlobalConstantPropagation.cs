bd6f526b594178fa3b64269576be6256af13cf79
Grgrgrrr <grgrgrrr29@gmail.com>
2016-12-21 23:13:44 +0300

    Draft of g;obal constant propagation

--- OptimizingCompilers2016/Library/Analysis/GlobalConstantPropagation.cs
+++ OptimizingCompilers2016/Library/Analysis/GlobalConstantPropagation.cs
@@ -9,22 +9,33 @@ using OptimizingCompilers2016.Library.ThreeAddressCode.Values;
 
 namespace OptimizingCompilers2016.Library.Analysis
 {
-    enum VariableValueType {
+    public enum VariableValueType {
         UNDEF,
         NAC,
         CONSTANT
     };
 
-    struct VariableValue {
+    public struct VariableValue {
         public VariableValueType type;
         public int constantValue;
     }
 
-    class GlobalConstantPropagation : BaseIterationAlgorithm<VariableMap>
+    public class GlobalConstantPropagation : BaseIterationAlgorithm<VariableMap>
     {
         List<BaseBlock> blocks = new List<BaseBlock>();
         public GlobalConstantPropagation(List<BaseBlock> _blocks) {
             blocks = _blocks;
+        }
+
+        public void runAnalys()
+        {
+            base.IterationAlgorithm(blocks);
+            foreach (var block in blocks){
+                Console.WriteLine("Block " + block.Name);
+                Console.WriteLine(outs[block].ToString());
+                Console.WriteLine("---------------------------------");
+            }
+
         }
 
         protected override VariableMap SetStartingSet()
@@ -47,7 +58,7 @@ namespace OptimizingCompilers2016.Library.Analysis
             }
 
 
-            Console.WriteLine(currentMap.ToString());
+            //Console.WriteLine(currentMap.ToString());
 
             return currentMap;
         }
@@ -101,36 +112,71 @@ namespace OptimizingCompilers2016.Library.Analysis
             foreach (var line in b.Commands) {
                 if (line.Operation.Equals(Operation.Assign))
                 {
-                    if ((line.RightOperand.Value == null) && (line.LeftOperand is NumericValue))
+                    if (line.RightOperand == null)
                     {
-                        VariableValue newValue = new VariableValue();
-                        newValue.type = VariableValueType.CONSTANT;
-                        newValue.constantValue = (line.LeftOperand as NumericValue).Value;
-                        newM.variableTable[line.Destination as IdentificatorValue] = newValue;
+                        if (line.LeftOperand is NumericValue)
+                        {
+                            VariableValue newValue = new VariableValue();
+                            newValue.type = VariableValueType.CONSTANT;
+                            newValue.constantValue = (line.LeftOperand as NumericValue).Value;
+                            newM.variableTable[line.Destination as IdentificatorValue] = newValue;
+                        }
+                        
                     }
                     else
                     {
-
+                        newM.variableTable[line.Destination as IdentificatorValue] =
+                            Calculate(line, x);
                     }
                 }
-                
-               
-
-
             }
-
-
-
-
-
             return newM;
         }
 
+        private VariableValue Calculate(IThreeAddressCode line, VariableMap currentTable) {
+            VariableValue newValue = new VariableValue();
+            VariableValue x = currentTable.variableTable[line.LeftOperand as IdentificatorValue];
+            VariableValue y = currentTable.variableTable[line.RightOperand as IdentificatorValue];
+            Operation op = line.Operation;
+
+            if (x.type.Equals(VariableValueType.CONSTANT) &&
+               (y.type.Equals(VariableValueType.CONSTANT)))
+            {
+                newValue.type = VariableValueType.CONSTANT;
+                newValue.constantValue = CalculateConstant(op, x.constantValue, y.constantValue);
+            }
+            else
+            if (x.type.Equals(VariableValueType.NAC) &&
+               (y.type.Equals(VariableValueType.NAC))) {
+                newValue.type = VariableValueType.NAC;
+            }
+            else
+            {
+                newValue.type = VariableValueType.UNDEF;
+            }
+            return newValue;
+        }
+
+        //TODO
+        private int CalculateConstant(Operation op, int x, int y) {
+            switch (op) {
+                case Operation.Plus:
+                    return x + y;
+                case Operation.Minus:
+                    return x - y;
+                case Operation.Mult:
+                    return x * y;
+                case Operation.Div:
+                    return x / y;
+                default:
+                    return 0;
+            }
+        }
 
     }
 
 
-    class VariableMap : ICloneable {
+    public class VariableMap : ICloneable {
 
         public Dictionary<IdentificatorValue, VariableValue> variableTable { get; }
 
@@ -185,8 +231,8 @@ namespace OptimizingCompilers2016.Library.Analysis
             foreach (KeyValuePair<IdentificatorValue, VariableValue> entry in variableTable)
             {
                 result += entry.Key + "     |     " 
-                    + entry.Value.type 
-                    + (entry.Value.type.Equals(VariableValueType.CONSTANT) ? entry.Value.constantValue.ToString() : "") 
+                    + entry.Value.type
+                    + (entry.Value.type.Equals(VariableValueType.CONSTANT) ? " : " + entry.Value.constantValue.ToString() : "") 
                     + "\n";
             }
 
