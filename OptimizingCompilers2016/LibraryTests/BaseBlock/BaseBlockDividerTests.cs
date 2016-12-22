a9531093e1415e203c1d29419248bee98e1e0197
Artem Konenko <yadummer@gmail.com>
2016-12-22 14:30:52 +0300

    Add almost empty project for unit tests

---
+++ OptimizingCompilers2016/LibraryTests/BaseBlock/BaseBlockDividerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OptimizingCompilers2016.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizingCompilers2016.Library.Tests
+{
+    [TestClass()]
+    public class BaseBlockDividerTests
+    {
+        [TestMethod()]
+        public void divideTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
